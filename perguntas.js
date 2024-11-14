28f27e3a649853b2e42ed8996d586c9209c1ccbb
JuhAndrade <jsouz5885@gmail.com>
2024-11-13 21:02:21 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,13 @@
 criaCartao(
     'Musica pop',
     'Quem é Biliie Eilish?',
-    'Billie Eilish é cantora e compositora,
-    nascida nos Estados Unidos.
-    Ganhou notoriedade e fam acom seu single "Ocean Eyes", 
-    escrito e produzido por seu irmão Finneas. No ano de 2020,
-    Billie foi convidada a fazer a música tema de um novo 007,
-    o último de Daniel Craig, onde escreveu e produziu a música "No Time to Die".'
+    'Billie Eilish é cantora e compositora,    nascida nos Estados Unidos.    Ganhou notoriedade e fam acom seu single "Ocean Eyes",     escrito e produzido por seu irmão Finneas. No ano de 2020,    Billie foi convidada a fazer a música tema de um novo 007,    o último de Daniel Craig, onde escreveu e produziu a música "No Time to Die".'
 )
 
 criaCartao(
     'Filmes',
     'O que são os vingadpores?',
-    'Vingadores ou Os Vingadores, The Avengers no original em inglês
-     são um grupo de super-heróis de história em quadrinhos publicados nos Estados Unidos pela editora Marvel Comics. 
-    O grupo também aparece em adaptações da Marvel para cinema, desenho animado e jogos eletrônicos.
-
-'
+    'Vingadores ou Os Vingadores, The Avengers no original em inglês     são um grupo de super-heróis de história em quadrinhos publicados nos Estados Unidos pela editora Marvel Comics.    O grupo também aparece em adaptações da Marvel para cinema, desenho animado e jogos eletrônicos.'
 )
 
 criaCartao(
